ac58efcff098fc5fd6ba5e5e9231bbaf004e0cb9
anzohra@outlook.com <anzohra@outlook.com>
2018-06-18 12:32:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,31 @@
-def create_an_empty_array
-  
+def instaniate_new_array
+	Array.new
 end
 
-def create_an_array
-  
+def array_with_two_elements
+	[1, 2]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def first_element(array)
+	array[0]
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def third_element(array)
+	array[2]
 end
 
-def remove_element_from_end_of_array(array)
-  
+def last_element(array)
+	array[-1]
 end
 
-def remove_element_from_start_of_array(array)
-  
+def first_element_with_array_methods(array)
+	array.first
 end
 
-def retrieve_element_from_index(array, index_number)
-  
+def last_element_with_array_methods(array)
+	array.last
 end
 
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+def length_of_array(array)
+	array.size
 end
